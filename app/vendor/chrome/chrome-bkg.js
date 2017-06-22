dc145afe95ab2b491f9264ceb5fb0beb633f926c
Vladimir Pasquier <vpasquier@nuxeo.com>
2017-06-22 14:11:38 -0400

    #10: add omnibox feature

--- app/vendor/chrome/chrome-bkg.js
+++ app/vendor/chrome/chrome-bkg.js
@@ -1,30 +1,25 @@
-var tabUrl;
-
+// Shortcuts
 chrome.commands.onCommand.addListener(function (command) {
     chrome.tabs.update({'url': 'http://chromium.org' + command}, function (tab) {
-        chrome.tabs.executeScript({
-            code: 'history.replaceState({}, "", " ");'
-        });
     });
 });
 
-function pageActionOnGES(tabInfo) {
-    chrome.pageAction.show(tabInfo.id);
-    return;
-}
-
-function getInfoForTab(tabs) {
-    if (tabs.length > 0) {
-        chrome.tabs.get(tabs[0].id, pageActionOnGES);
-    }
-}
-
-function onChange(tabInfo) {
-    chrome.tabs.query({lastFocusedWindow: true, active: true}, getInfoForTab);
-};
-
-var target = '<all_urls>';
-chrome.webRequest.onCompleted.addListener(onChange, {urls: [target]});
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.tabs.get(activeInfo.tabId, pageActionOnGES);
+// Omnibox
+chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+    text = text.replace(' ', '');
+    var suggestions = [];
+    suggestions.push({content: 'Search with DuckDuckGo: ' + text, description: 'https://duckduckgo.com/?q=' + text});
+    chrome.omnibox.setDefaultSuggestion({description: suggestions[0].description});
+    suggestions.shift();
+    suggest(suggestions);
 });
+
+chrome.omnibox.onInputEntered.addListener(function (text) {
+    navigate('https://duckduckgo.com/?q=' + text + '&ia=web');
+});
+
+function navigate(url) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.update(tabs[0].id, {url: url});
+    });
+}
